0104cae3cefecf0930b1156bf96281fb543a8630
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-17 11:37:00 +0100

    added notes about arrow function in class

--- Drawing Animation/index.js
+++ Drawing Animation/index.js
@@ -4,18 +4,29 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener(
-      'click',
-      this.start
-    ); /* 2
-    but when we call this one */
+    this.startButton.addEventListener('click', this.start);
   }
+  /* start: function() {
+    this.anothermethod();
+    console.log('Time to start the timer!');
+  } THIS IS HOW THE ENGINE SHOW THIS FUNCTION */
+
+  /* we can solve this issue in 2 ways. 
+  start=() => {
+    this.anothermethod(); this will work because under the hood babel
+     put this code to construcor and call there, as a result this will be class in there
+    console.log('Time to start the timer!');
+  } */
+
+  /* another way is below */
+
+  /* this.startButton.addEventListener('click', this.start.bind(this)); */
 
   start() {
-    this.anothermethod(); /* 3 this won't work because in here this points to button and 
-    it cannot find anothermethod in button */
+    this.anothermethod();
     console.log('Time to start the timer!');
   }
+  
   anothermethod() {
     console.log('we need this method');
   }
@@ -26,65 +37,3 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
-// timer.start();
-/*1 this code will work immitiately and without problem because this in here 
-has refernce, it knows it should point to timer. */
-
-// console.log(this); /* both will be window */
-// const sayhello = () => {
-//   console.log(
-//     this
-//   ); /*  because this is arrow, when we have arrow,
-//   this inside of it is the same with what is this outside of function */
-// };
-// sayhello();
-
-// const hello = {
-//   saybye() {
-//     console.log(this); /* both points to hello object  */
-//     const saybye2 = () => {
-//       console.log(this); /* both points to hello object  */
-//     };
-//     saybye2();
-//   }
-// };
-// hello.saybye();
-
-// function sayhello() {
-//   console.log(this);
-// }
-
-// sayhello.call({
-//   a: '2'
-// }); /* if we use call, bind, apply
-// we say this inside of call points to first parametr of call method
-//  */
-
-// function sayhello() {
-//   console.log(this);
-// }
-
-// // sayhello(); /* global window object */
-
-// const color = {
-//   method: sayhello
-// };
-
-// color.method(); /* this points to color */
-
-const color = {
-  sayhello() {
-    console.log(this.a);
-  }
-};
-// const color = {
-//   sayhello() {
-//     console.log(this);
-//   }
-// };
-const anotherObject = {
-  a: 'hello'
-};
-anotherObject.sayhelloatleast = color.sayhello;
-
-anotherObject.sayhelloatleast(); /* this points to anotherObject because it is left of function */
--- Notes/Notes section 20.md
+++ Notes/Notes section 20.md
@@ -1,10 +1,12 @@
 In order to determine this we need to ask 3 question.
 
-1. Do we use arrow function, if yes it means this points to where in above line this
-   points. it can be window, cna be any object etc
+1. Do we use arrow function, if yes it means this points to where we can write console.log(this) validly this points there in above arrow function. it can be window, cna be any object etc
+
 2. Do we use call, bind, apply? if yes what is first argument of call, bind, apply. I
    It means this point there.
 3. the rest cases, this points left of function
+
+- When we define arrow function in class, babel put that in constructor under the hood.
 
 ```
 sayhello() <!-- this ponits to global -->
