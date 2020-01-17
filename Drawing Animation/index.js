837267d6bfde7940c7476718abcbb49088668aa6
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-17 02:07:43 +0100

    notes about this in arrow function

--- Drawing Animation/index.js
+++ Drawing Animation/index.js
@@ -4,11 +4,20 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener('click', this.start);
+    this.startButton.addEventListener(
+      'click',
+      this.start
+    ); /* 2
+    but when we call this one */
   }
 
   start() {
+    this.anothermethod(); /* 3 this won't work because in here this points to button and 
+    it cannot find anothermethod in button */
     console.log('Time to start the timer!');
+  }
+  anothermethod() {
+    console.log('we need this method');
   }
 }
 
@@ -17,3 +26,15 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
+// timer.start();
+/*1 this code will work immitiately and without problem because this in here 
+has refernce, it knows it should point to timer. */
+
+console.log(this); /* both will be window */
+const sayhello = () => {
+  console.log(
+    this
+  ); /*  because this is arrow, when we have arrow, 
+  this inside of it is the same with what is this outside of function */
+};
+sayhello();
