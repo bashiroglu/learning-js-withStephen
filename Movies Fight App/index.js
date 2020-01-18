e84834df620782fbfac182fdf55bc34b7d20df15
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-18 21:45:54 +0100

    added funct. of displaying movies

--- Movies Fight App/index.js
+++ Movies Fight App/index.js
@@ -13,21 +13,52 @@ const FetchSearchResult = async searchedKeyword => {
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await FetchSearchResult(event.target.value);
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+  resultsWrapper.innerHTML = '';
+  dropdown.classList.add('is-active');
+
   for (let movie of movies) {
-    const div = document.createElement('div');
+    const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-    div.innerHTML = `
-    <img src="${movie.Poster}" alt="${movie.Title}">
-      <h1>${movie.Title}</h1>
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
     `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
 
-    const id = document.getElementById('target');
-    id.appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
