7568b948c9548b5953046ee06559103c21665bbc
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-18 13:41:55 +0100

    finished Timer with Drawing Animation, added notes

---
+++ Drawing Animation/Timer.js
@@ -0,0 +1,50 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      //we do like this, because we don't know it will be exist for sure
+      this.onStart(this.timeRemaining);
+    }
+    this.tick();
+    this.intervalhandler = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  pause = () => {
+    clearInterval(this.intervalhandler);
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
--- Notes/Notes section 20.md
+++ Notes/Notes section 20.md
@@ -12,3 +12,36 @@ In order to determine this we need to ask 3 question.
 sayhello() <!-- this ponits to global -->
 color.sayhello() <!-- this ponits to color object -->
 ```
+
+- when we use getter method we don't have to execute the function
+
+```
+  start = () => {
+    let remainingTime = this.timeRemaining;
+  };
+```
+
+- when we use setter method we don't have to give argument in normal syntax
+
+```
+  start = () => {
+    this.timeRemaining = this.timeRemaining - 1;
+  };
+```
+
+we can communicate with our class by adding aditional function arguments to our class in process of creating instance of it.
+
+```
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer started');
+  },
+  onTick() {
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 50;
+  },
+  onComplete() {
+    console.log('Timer is completed');
+  }
+});
+```
