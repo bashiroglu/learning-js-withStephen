1bed7656645ea049c27d9c3ad2b8b283f8506531
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-18 22:12:17 +0100

    added displaing movie details funct.

--- Movies Fight App/index.js
+++ Movies Fight App/index.js
@@ -49,6 +49,7 @@ const onInput = async event => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovideSelect(movie);
     });
 
     resultsWrapper.appendChild(option);
@@ -62,3 +63,52 @@ document.addEventListener('click', event => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovideSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '41e8565e',
+      i: movie.imdbID
+    }
+  });
+
+  document.querySelector('.summary').innerHTML = movieTemplate(response.data);
+};
+const movieTemplate = movieDetail => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
+};
