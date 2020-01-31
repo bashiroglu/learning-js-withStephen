4f9ae5da53f85b6dd1c803cd6c6198b47ce66ea0
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-31 23:55:03 +0100

    add update funct.

--- E-commerce/repositories/users.js
+++ E-commerce/repositories/users.js
@@ -39,11 +39,23 @@ class UsersRepository {
   async deleteOne(id) {
     const records = await this.getAll();
     const newRecords = records.filter(user => user.id !== id);
-    await this.writeAll(newRecords)
+    await this.writeAll(newRecords);
     return newRecords;
   }
   getRandomId() {
     return crypto.randomBytes(4).toString('hex');
+  }
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(user => user.id === id);
+    if (!record) {
+      throw new Error('we cound not find this user');
+    }
+    console.log(records);
+    console.log(record);
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
   }
 }
 
@@ -51,7 +63,9 @@ const test = async () => {
   const repo = new UsersRepository('users.json');
   // await repo.create({ email: 'bashiroghlu@gmail.com', password: '12345' });
   // let user = await repo.getOne('a4963f47');
-  let user = await repo.deleteOne('75d2a427');
+  let user = await repo.update('ec235127', {
+    password: 'ec235127'
+  });
   console.log(user);
 };
 
