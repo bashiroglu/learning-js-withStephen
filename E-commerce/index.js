ff2defb0aa2eebd8d37ff8e85a5fe83f27744cab
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-01 13:42:09 +0100

    add funct. of adding session and session data

--- E-commerce/index.js
+++ E-commerce/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const repo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['mysecretkey']
+  })
+);
 
 app.get('/', (req, res) => {
   res.send(`
     <div>
+    ${req.session.userId}
       <form method="POST">
         <input name="email" placeholder="email" />
         <input name="password" placeholder="password" />
@@ -30,6 +37,10 @@ app.post('/', async (req, res) => {
   if (password !== passwordConfirmation) {
     return res.send('password should be the same');
   }
+  const user = await repo.create({ email, password });
+
+  req.session.userId = user.id;
+
   res.send('account created');
 });
 
