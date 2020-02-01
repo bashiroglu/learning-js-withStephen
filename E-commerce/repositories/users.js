4ac54cda1f8c320a5f6354bf856d127841eee728
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-01 15:04:51 +0100

    add funtc. of hashing password and storing it

--- E-commerce/repositories/users.js
+++ E-commerce/repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -23,9 +26,17 @@ class UsersRepository {
   async create(attrs) {
     attrs.id = this.getRandomId();
     const records = await this.getAll();
-    records.push(attrs);
+
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buff = await scrypt(attrs.password, salt, 64);
+    let record = {
+      ...attrs,
+      password: `${buff.toString('hex')}.${salt}`
+    };
+
+    records.push(record);
     await this.writeAll(records);
-    return attrs;
+    return record;
   }
   async writeAll(records) {
     await fs.promises.writeFile(
