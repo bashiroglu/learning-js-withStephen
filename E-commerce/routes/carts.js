bbe49f29057b90331413c6b98e8e228d8ff0bcdc
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-04 14:05:13 +0100

    add funct. of increasing quantity of product

--- E-commerce/routes/carts.js
+++ E-commerce/routes/carts.js
@@ -5,10 +5,23 @@ const router = express.Router();
 router.post('/cart/products', async (req, res) => {
   let cart;
   if (!req.session.cartId) {
-    cart = await cartsRepo.create({ items: [] });
+    cart = await cartsRepo.create({
+      items: [{ id: req.body.productId, quantity: 1 }]
+    });
     req.session.cartId = cart.id;
   } else {
     cart = await cartsRepo.getOne(req.session.cartId);
+    const productId = req.body.productId;
+    const existingProduct = cart.items.find(item => item.id === productId);
+    if (existingProduct) {
+      existingProduct.quantity++;
+    } else {
+      cart.items.push({ id: req.body.productId, quantity: 1 });
+    }
+    await cartsRepo.update(cart.id, {
+      items: cart.items
+    });
+    // req.session.cartId = null;
   }
   console.log(cart);
 
